2c59116ba23ef16dc8edbf7c5b8adf44df4fe0ac
gaocan_wang <117655210+GaocanWang@users.noreply.github.com>
2024-10-11 13:52:42 -0400

    Added basic stuff that will make adding features easier later on

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -15,6 +15,7 @@ var direction_history = []
 @onready var sprite : Sprite2D = $Sprite2D
 @onready var state_machine : PlayerStateMachine = $StateMachine
 
+signal DirectionChanged( new_direction: Vector2 )
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -52,6 +53,7 @@ func SetDirection() -> bool:
 		return false
 	
 	cardinal_direction = new_dir
+	DirectionChanged.emit( new_dir )
 	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	return true
 
