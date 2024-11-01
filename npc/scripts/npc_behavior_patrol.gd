864e618db662d09f6b6d910a4e50ea0120f87e76
gaocan_wang <117655210+GaocanWang@users.noreply.github.com>
2024-11-01 19:12:53 -0400

    added basic dialog

--- npc/scripts/npc_behavior_patrol.gd
+++ npc/scripts/npc_behavior_patrol.gd
@@ -25,7 +25,7 @@ func _ready() -> void:
 
 
 
-func _process(delta: float) -> void:
+func _process( _delta: float ) -> void:
 	if Engine.is_editor_hint():
 		return
 	if npc.global_position.distance_to( target.target_position ) < 1:
